0a3ac1bfeb321af28e40baa98635b80a3a7c9f15
kkomorowski236562 <229917@edu.p.lodz.pl>
2024-03-17 14:19:18 +0100

    first method with test

---
+++ tpw/tpw0/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpw
+{
+    public class Calculator
+    {
+        public int add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
